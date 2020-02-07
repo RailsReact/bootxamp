d1477bd9be949fb2b2821fab8c7dfb0fa0c7f6e8
excavator <str_token@mail.ru>
2020-02-07 16:23:06 +0300

    create pages

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return(
+    <div>
+      <h1>My Blog</h1>
+      <p>Post will show up here later on ...</p>
+    </div>
+  )
+}
+
+export default BlogPage
+// localhost:8000/blog
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactPage = () => {
+  return(
+    <div>
+      <h1>My Contact</h1>
+      <p>Here is the my contact ... </p>
+    </div>
+  )
+}
+
+export default ContactPage
