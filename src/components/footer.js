f370923310385092f1555e582ccd2466775e9506
excavator <str_token@mail.ru>
2020-02-09 15:23:49 +0300

    use components

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () =>{
+  return(
+    <footer>
+      <hr />
+      <p>Created by Excavator, © 2020</p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Header from '../components/header'
+import Footer from '../components/footer'
 
 const AboutPage = () => {
   return(
     <div>
+      <Header />
       <h1>About Me</h1>
       <p>Here is something about me ...</p>
       <p>Need to <Link to="/contact">contact me.</Link></p>
+      <Footer />
     </div>
   )  
 }
